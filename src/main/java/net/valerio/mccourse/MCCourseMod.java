b92a365a1fe8100556ab529a2d4a59b614ce3c98
stefano liviero <steveliv2008@gmail.com>
2025-10-15 08:50:06 +0200

    ho aggiunto un commento nella classe MCCourseMod.java

--- src/main/java/net/valerio/mccourse/MCCourseMod.java
+++ src/main/java/net/valerio/mccourse/MCCourseMod.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
 // L'annotazione @Mod comunica a Forge che questa è la classe principale della MOD
 @Mod(MCCourseMod.MOD_ID)
 
-//Commento di Stefano Liviero
+//Commento Di Stefano Liviero
 public class MCCourseMod
 {
     // Definisce il MOD ID
