81d9bf3556bb1a7dd226d05c0d9ccde02d5131c3
micol <righimicky08@gmail.com>
2025-10-15 08:38:42 +0200

    Aggiunto un commento alla classe MCCourseMod

--- src/main/java/net/valerio/mccourse/MCCourseMod.java
+++ src/main/java/net/valerio/mccourse/MCCourseMod.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
 // L'annotazione @Mod comunica a Forge che questa è la classe principale della MOD
 @Mod(MCCourseMod.MOD_ID)
 
-//
+//ccommmento di micolRRIGHI
 public class MCCourseMod
 {
     // Definisce il MOD ID
