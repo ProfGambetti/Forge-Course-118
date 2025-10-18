1df56f6e9ec6bea0370bd24fb00a288375117c17
ChristianRizzi9 <christian.rizzi2008@gmai.com>
2025-10-18 21:06:48 +0200

    Implementazione nel gioco dell'armatura di cobalto completa (elmo, corazza, pantaloni e scarpe) tutti e 4 gli item craftabili nella crafting table, texture fatte tramite l'uttilizzo di Nova Skin Texture Pack Creator.
    Aggiunta quindi della classe ArmaturaCobalto, modifica codice classe ModItems, aggiunta di file json per il funzionamento dei crafting, modifica codice file json di traduzione en_us.json, aggiunte texture item in inventario e item indossatti sul personagggio

---
+++ src/main/java/net/valerio/mccourse/item/ArmaturaCobalto.java
@@ -0,0 +1,80 @@
+package net.valerio.mccourse.item;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.valerio.mccourse.MCCourseMod;
+
+import java.util.function.Supplier;
+
+public class ArmaturaCobalto implements ArmorMaterial {
+    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+    private static final int[] SLOT_PROTECTIONS = new int[]{5, 8, 10, 5}; // DA QUESTO ARRAY MODIFICO I VALORI DI ARMATURA AGGIUNTIVA DEI 4 SINGOLI PEZZI
+
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    private static final String[] TEXTURE_NAMES = {
+            "cobalt_layer_1",  // per elmo, pantaloni, scarpe
+            "cobalt_layer_2"   // per corazza
+    };
+
+    public ArmaturaCobalto() {
+        this.name = "cobalt";
+        this.durabilityMultiplier = 25;
+        this.enchantmentValue = 15;
+        this.sound = SoundEvents.ARMOR_EQUIP_IRON;
+        this.toughness = 2.0F;
+        this.knockbackResistance = 0.0F;
+        this.repairIngredient = new LazyLoadedValue<>(() -> Ingredient.of(ModItems.COBALT_INGOT.get()));
+    }
+
+    @Override
+    public int getDurabilityForSlot(EquipmentSlot slot) {
+        return HEALTH_PER_SLOT[slot.getIndex()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getDefenseForSlot(EquipmentSlot slot) {
+        return SLOT_PROTECTIONS[slot.getIndex()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return MCCourseMod.MOD_ID + ":" + this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
+
