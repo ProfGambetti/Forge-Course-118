49b96e4e8630e8d5b54f935ec7b6092115333710
Francesco L <francescolacava008@gmail.com>
2025-10-21 20:10:09 +0200

    Ho aggiunto l'arma personalizzata(death sickle)

---
+++ src/main/java/net/valerio/mccourse/item/DeathSickle.java
@@ -0,0 +1,26 @@
+package net.valerio.mccourse.item;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class DeathSickle extends Item{
+
+    public DeathSickle(Properties props) {
+        super(props.durability(0));
+    }
+}
