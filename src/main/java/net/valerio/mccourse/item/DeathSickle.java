b619b0b05023f2853136ac3be38b78c060a110e6
Francesco L <francescolacava008@gmail.com>
2025-10-22 16:40:30 +0200

    Ho aggiunto l'effetto personalizzato della falce

--- src/main/java/net/valerio/mccourse/item/DeathSickle.java
+++ src/main/java/net/valerio/mccourse/item/DeathSickle.java
@@ -20,6 +20,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.level.Explosion;
+import java.util.Timer;
+import java.util.TimerTask;
+import net.minecraft.world.damagesource.DamageSource;
 
 
 public class DeathSickle extends Item {
@@ -51,6 +54,10 @@ public class DeathSickle extends Item {
 
     }
 
+    //In questo metodo, che viene richiamato quando colpisco un'entità vivente con la falce, faccio levitare l'entità
+    //per alcuni secondi e successivamente creo un'esplosione alle sue coordinate: questa esplosione
+    //non danneggia blocchi, player o altre entità vicine ma serve solo come effetto visivo. L'entità
+    //in realtà scompare, facendo sembrare che sia morta dall'esplosione
     @Override
     public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         if (!attacker.level.isClientSide) {
@@ -63,19 +70,25 @@ public class DeathSickle extends Item {
             MobEffectInstance lev = new MobEffectInstance(MobEffects.LEVITATION, durata, livello);
             boolean applied = target.addEffect(lev);
 
-            level.explode(
-                    target,                   // entità che causa l’esplosione
-                    target.getX(),             // coordinata X
-                    target.getY(),             // coordinata Y
-                    target.getZ(),             // coordinata Z
-                    3.0F,                      // potenza dell’esplosione (4.0 = TNT)
-                    Explosion.BlockInteraction.NONE        // tipo di esplosione (TNT, NONE, DESTROY)
-            );
-
             if (attacker instanceof Player player) {
                 player.displayClientMessage(new TextComponent("✨ Hai fatto levitare ed esplodere il nemico ✨"), true);
             }
+            Timer timer = new Timer();
 
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    level.explode(
+                            target,                    // entità che causa l’esplosione
+                            target.getX(),             // coordinata X
+                            target.getY(),             // coordinata Y
+                            target.getZ(),             // coordinata Z
+                            6.0F,                      // potenza dell’esplosione (4.0 = TNT)
+                            Explosion.BlockInteraction.NONE        // tipo di esplosione (TNT, NONE, DESTROY)
+                    );
+                    target.hurt(DamageSource.explosion(attacker), Float.MAX_VALUE);
+                }
+            }, 3200);//Timer dopo il quale eseguire l'esplosione(3200 = 3,20 secondi)
         }
         return true;
     }
