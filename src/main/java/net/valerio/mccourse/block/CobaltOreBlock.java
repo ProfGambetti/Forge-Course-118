a3a25d4d5ef70721ceea4093ce8e15f27cc9d3c7
stefano liviero <steveliv2008@gmail.com>
2025-10-30 13:59:53 +0100

    ho aggiunto l'ore di cobalto, il piccone di cobalto,la spada di cobalto, il cobalto grezzo, tutti i crafting collegati, la cottura del cobalto grezzo, il mining di tutti i blocchi che ho implementato, un comando che trova il blocco di cobalto più vicino.

--- src/main/java/net/valerio/mccourse/block/CobaltOreBlock.java
+++ src/main/java/net/valerio/mccourse/block/CobaltOreBlock.java
@@ -1,5 +1,5 @@
 package net.valerio.mccourse.block;
-
+//ciao
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
