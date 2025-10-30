e862433d645d6e2d057929c2542f67fae44933bd
yuval <yuvalentina68@gmail.com>
2025-10-30 02:10:08 +0100

    pennello che cambia colore delle pecore

--- src/main/java/net/valerio/mccourse/item/ModItems.java
+++ src/main/java/net/valerio/mccourse/item/ModItems.java
@@ -32,6 +32,7 @@ public class ModItems {
 
     // DA NOTARE CHE DEVO CREARE UN OGGETTO DELLA CLASSE MagicStick che ho creato io
     public static final RegistryObject<Item> MAGIC_STICK = ITEMS.register("magic_stick", () -> new MagicStick(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> BRUSH = ITEMS.register("brush", () -> new Brush(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     // Collega gli elementi creati con il ciclo di eventi di FORGE
     public static void register(IEventBus eventBus)
